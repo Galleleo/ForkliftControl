d9529a53a2687bf20fb1c5e033e67c8448138d6b
Alexander Gallwitz <a.gallwitz@t-online.de>
2025-08-26 23:42:58 +0700

    Fix frontend git repo

--- obj/Debug/net8.0/ForkliftControl.AssemblyInfo.cs
+++ obj/Debug/net8.0/ForkliftControl.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ForkliftControl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6ab6b713c6fc17f2b3cf1a71a40f65f9c369e3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5072389dce9bd540fddfb169eb98283a25789b0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ForkliftControl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ForkliftControl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
